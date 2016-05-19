d85f97574a813ba27e8a4132042d50afaabc77f9
Bruce Ediger <bediger@stratigery.com>
2016-05-19 08:44:08 -0600

    Mostly working version.

--- squava.go
+++ squava.go
@@ -10,30 +10,80 @@ type Board [5][5]int
 const WIN = 1000
 const LOSS = -1000
 
-var max_depth int = 4
+var max_depth int = 6
 
 func main() {
 	var bd Board
-	var max int = LOSS
+	var max int
 	var move [2]int
+	var winner int
+	var end_of_game bool
 
-	for i, row := range bd {
-		for j, marker := range row {
-			if marker == 0 {
-				bd[i][j] = 1
-				val := alphabeta(&bd, 1, -1, LOSS, WIN)
-				bd[i][j] = 0
-				fmt.Printf("Move (%d, %d) value %d\n", i, j, val)
-				if val > max {
-					move[0] = i
-					move[1] = j
-					max = val
+	for {
+		var l, m int
+
+		for looping := true; looping; {
+			fmt.Printf("Move> ")
+			_, err := fmt.Scanf("%d %d\n", &l, &m)
+			if err != nil {
+				fmt.Printf("Failed to read: %v\n", err)
+				os.Exit(1)
+			}
+			if bd[l][m] == 0 {
+				looping = false
+			}
+		}
+
+		bd[l][m] = -1
+
+		winner, end_of_game = check_winner(&bd)
+
+		if end_of_game {
+			break
+		}
+
+		max = LOSS
+
+		for i, row := range bd {
+			for j, mark := range row {
+				if mark == 0 {
+					bd[i][j] = 1
+					val := alphabeta(&bd, 1, -1, LOSS, WIN)
+					bd[i][j] = 0
+					/// fmt.Printf("Move (%d, %d) value %d\n", i, j, val)
+					if val > max {
+						move[0] = i
+						move[1] = j
+						max = val
+					}
 				}
 			}
 		}
+
+		fmt.Printf("My move: (%d, %d)\n", move[0], move[1])
+
+		bd[move[0]][move[1]] = 1
+
+		print_board(&bd)
+
+		winner, end_of_game = check_winner(&bd)
+
+		if end_of_game {
+			break
+		}
 	}
 
-	bd[move[0]][move[1]] = 1
+	var phrase string
+	switch winner {
+	case 1:
+		phrase = "X wins\n"
+	case 0:
+		phrase = "Cat wins\n"
+	case -1:
+		phrase = "O wins\n"
+	}
+
+	fmt.Printf(phrase)
 
 	os.Exit(0)
 }
@@ -101,22 +151,26 @@ func alphabeta(bd *Board, ply int, player int, alpha int, beta int) (value int) 
 	return value
 }
 
-var scores [][]int = [][]int{
-	[]int{1, 0, 0, 0, 1},
-	[]int{0, 2, 2, 2, 0},
-	[]int{0, 2, 3, 2, 0},
-	[]int{0, 2, 2, 2, 0},
-	[]int{1, 0, 0, 0, 1},
+func print_board(bd *Board) {
+	fmt.Printf("   0 1 2 3 4\n")
+	for i, row := range bd {
+		fmt.Printf("%d  ", i)
+		for _, v := range row {
+			var marker string
+			switch v {
+			case 1:
+				marker = "X"
+			case -1:
+				marker = "O"
+			case 0:
+				marker = "_"
+			}
+			fmt.Printf("%s ", marker)
+		}
+		fmt.Printf("\n")
+	}
 }
 
-func static_value(bd *Board) (score int) {
-	for i, row := range bd {
-		for j, _ := range row {
-			score += bd[i][j] * scores[i][j]
-		}
-	}
-	return score
-}
 
 var losing_triplets [][][]int = [][][]int{
 	[][]int{[]int{0, 0}, []int{1, 0}, []int{2, 0}},
@@ -200,16 +254,6 @@ var winning_quads [][][]int = [][][]int{
 }
 
 func check_winner(bd *Board) (winner int, end_of_game bool) {
-	for _, triplet := range losing_triplets {
-		sum := bd[triplet[0][0]][triplet[0][1]]
-		sum += bd[triplet[1][0]][triplet[1][1]]
-		sum += bd[triplet[2][0]][triplet[2][1]]
-
-		if sum == 3 || sum == -3 {
-			loser := sum / 3
-			return -loser, true
-		}
-	}
 	for _, quad := range winning_quads {
 		sum := bd[quad[0][0]][quad[0][1]]
 		sum += bd[quad[1][0]][quad[1][1]]
@@ -221,6 +265,16 @@ func check_winner(bd *Board) (winner int, end_of_game bool) {
 			return winner, true
 		}
 	}
+	for _, triplet := range losing_triplets {
+		sum := bd[triplet[0][0]][triplet[0][1]]
+		sum += bd[triplet[1][0]][triplet[1][1]]
+		sum += bd[triplet[2][0]][triplet[2][1]]
+
+		if sum == 3 || sum == -3 {
+			loser := sum / 3
+			return -loser, true
+		}
+	}
 	for _, row := range bd {
 		for _, val := range row {
 			if val == 0 {
@@ -230,4 +284,31 @@ func check_winner(bd *Board) (winner int, end_of_game bool) {
 	}
 	// Get here, all 25 spots on board filled, no winning quadruplet
 	return 0, true
+}
+
+var scores [][]int = [][]int{
+	[]int{0, 1, 2, 1, 0},
+	[]int{1, 2, 2, 2, 1},
+	[]int{2, 2, 3, 2, 2},
+	[]int{1, 2, 2, 2, 1},
+	[]int{0, 1, 2, 1, 0},
+}
+
+func static_value(bd *Board) (score int) {
+	for i, row := range bd {
+		for j, _ := range row {
+			score += bd[i][j] * scores[i][j]
+		}
+	}
+	for _, quad := range winning_quads {
+		sum := bd[quad[0][0]][quad[0][1]]
+		sum += bd[quad[1][0]][quad[1][1]]
+		sum += bd[quad[2][0]][quad[2][1]]
+		sum += bd[quad[3][0]][quad[3][1]]
+
+		if sum == 3 || sum == -3 {
+			score += sum * 10
+		}
+	}
+	return score
 }
