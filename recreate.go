ccf940c65bc361c69929481e9de96a75a08f4287
Bruce Ediger <bediger8@gmail.com>
2022-12-24 07:39:19 -0700

    Rework interactive game re-creation
    Allow 1 line or multiple line game inputs

--- recreate.go
+++ recreate.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
@@ -18,44 +16,37 @@ func main() {
 		board[i] = []rune{'_', '_', '_', '_', '_'}
 	}
 
-	fin, err := os.Open(os.Args[1])
+	buf, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer fin.Close()
+	var moves [][]byte
+	lines := bytes.Count(buf, []byte{'\n'})
+	if lines > 1 {
+		// assume 1 move per line
+		moves = bytes.Split(buf, []byte{'\n'})
+	} else {
+		// assume all moves on one line
+		moves = bytes.Split(buf, []byte{' '})
+	}
 
-	scanner := bufio.NewScanner(fin)
+	moveCounter := 0
 
-	lineCounter := 0
+	for _, move := range moves {
+		moveCounter++
 
-	for scanner.Scan() {
-		lineCounter++
-		line := scanner.Text()
+		if moveCounter > 25 {
+			break
+		}
 
-		fields := strings.Split(line, ",")
-		if len(fields) != 2 {
-			fmt.Fprintf(os.Stderr, "Line %d, %q, problem\n", lineCounter, line)
+		n, m, useIt := finagleMove(move, moveCounter)
+		if !useIt {
 			continue
 		}
-		if len(fields[0]) == 2 {
-			fields[0] = fields[0][0:1]
-		}
-		if len(fields[1]) == 2 {
-			fields[1] = fields[1][0:1]
-		}
-		n, nerr := strconv.Atoi(fields[0])
-		if nerr != nil {
-			fmt.Fprintf(os.Stderr, "Line %d, %q, problem with 1st field %v\n", lineCounter, line, nerr)
-			continue
-		}
-		m, merr := strconv.Atoi(fields[1])
-		if merr != nil {
-			fmt.Fprintf(os.Stderr, "Line %d, %q, problem with 2nd field %v\n", lineCounter, line, merr)
-			continue
-		}
-		fmt.Printf("%c move %d,%d\n", markers[lineCounter%2], n, m)
-		board[n][m] = markers[lineCounter%2]
+
+		fmt.Printf("%c move %d,%d\n", markers[moveCounter%2], n, m)
+		board[n][m] = markers[moveCounter%2]
 
 		for i := 0; i < 5; i++ {
 			for _, marker := range board[i] {
@@ -69,8 +60,37 @@ func main() {
 			log.Print(err)
 		}
 	}
+}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("problem line %d: %v", lineCounter, err)
+func finagleMove(move []byte, moveCounter int) (int, int, bool) {
+
+	fields := bytes.Split(move, []byte{','})
+
+	if len(fields) != 2 {
+		fmt.Fprintf(os.Stderr, "Move %d, %q, problem\n", moveCounter, string(move))
+		return 0, 0, false
 	}
+
+	if len(fields[0]) == 2 {
+		fields[0] = fields[0][0:1]
+	}
+	if len(fields[1]) == 2 {
+		fields[1] = fields[1][0:1]
+	}
+
+	if fields[0][0] < '0' || fields[0][0] > '4' {
+		fmt.Fprintf(os.Stderr, "Move %d, %q, problem with 1st field\n", moveCounter, string(move))
+		return 0, 0, false
+	}
+
+	n := int(fields[0][0] - '0')
+
+	if fields[1][0] < '0' || fields[1][0] > '4' {
+		fmt.Fprintf(os.Stderr, "Move %d, %q, problem with 2nd field\n", moveCounter, string(move))
+		return 0, 0, false
+	}
+
+	m := int(fields[1][0] - '0')
+
+	return n, m, true
 }
